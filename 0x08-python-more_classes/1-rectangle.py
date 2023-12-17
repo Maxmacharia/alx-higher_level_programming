eeb82e0f73079eb663fe09fbceb03a5e68fb9366
Maxmacharia <maxwellmaina5194@gmail.com>
2023-12-17 00:58:48 -0800

    class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,23 +6,11 @@
 
 class Rectangle:
     """Defining an instance"""
-    def __init__(self, width=0, height=0):
-        self.__width = width
+    def __init__(self, height=0, width=0):
         self.__height = height
+        self.__width = width
 
     """Defining a private instance attribute"""
-
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
 
     @property
     def height(self):
@@ -35,3 +23,15 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
