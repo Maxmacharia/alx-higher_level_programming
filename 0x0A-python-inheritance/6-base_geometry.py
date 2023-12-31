7fd98b234b8003fbc2a94be48df79badb1d7b307
Maxmacharia <maxwellmaina5194@gmail.com>
2023-12-31 07:19:15 -0800

    class BaseGeometry (based on 5-base_geometry.py)

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 
-
 """A module that raise an exeception"""
 
 
 class BaseGeometry:
     """Defining a public attribute instance"""
     def area(self):
+        """Raising an exception"""
         raise Exception("area() is not implemented")
